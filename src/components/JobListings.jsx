ca6cd8660d5ca236327e1c6a769e16817087d9dc
Tarif24 <tarif24@hotmail.com>
2025-01-25 02:30:36 -0500

    Getting the job listings from an api and added a spinner when fetching from the api

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import jobs from "../jobs.json";
+import React, { useState, useEffect } from "react";
+import Spinner from "./Spinner";
 import JobListing from "./JobListing";
 
-const JobListings = ({isHome = false}) => {
-    
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+const JobListings = ({ isHome = false }) => {
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const res = await fetch("http://localhost:3001/jobs");
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.error("Error fetching data", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJobs();
+    }, []);
 
     return (
         <section className="bg-blue-50 px-4 py-10">
@@ -12,11 +28,15 @@ const JobListings = ({isHome = false}) => {
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                     {isHome ? "Recent Jobs" : "Browse Jobs"}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListings.map((job) => (
-                        <JobListing key={job.id} job={job} />
-                    ))}
-                </div>
+                {loading ? (
+                    <Spinner loading={loading} />
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+                        ))}
+                    </div>
+                )}
             </div>
         </section>
     );
